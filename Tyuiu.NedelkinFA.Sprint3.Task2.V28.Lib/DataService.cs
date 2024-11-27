3b0ad0331ec5122a4044004a05777792b20e1a3f
Федя Неделькин <fffedosss898@gmail.com>
2024-11-27 20:26:12 +0500

    рп

--- Tyuiu.NedelkinFA.Sprint3.Task2.V28.Lib/DataService.cs
+++ Tyuiu.NedelkinFA.Sprint3.Task2.V28.Lib/DataService.cs
@@ -1,20 +1,19 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint3;
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
 namespace Tyuiu.NedelkinFA.Sprint3.Task2.V28.Lib
 {
-    public class DataService
+    public class DataService : ISprint3Task2V28
     {
         public double GetMultiplySeries(int startValue, int stopValue)
         {
-            double result = 1.0;
-            double correctionFactor = -0.15;
+            double result = 1.0; 
             do
             {
-                double term = Math.Pow(2, startValue) / (startValue + 1.0);
-                result *= term;
+                result *= (1 / Math.Sin(startValue)) + 2.5;
                 startValue++;
             } while (startValue <= stopValue);
-            result *= Math.Cos(1.8);
-            result *= correctionFactor;
+
             return Math.Round(result, 3);
         }
     }
--- Tyuiu.NedelkinFA.Sprint3.Task2.V28.Test/DataServiceTest.cs
+++ Tyuiu.NedelkinFA.Sprint3.Task2.V28.Test/DataServiceTest.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.NedelkinFA.Sprint3.Task2.V28.Test
             int startValue = 1;
             int stopValue = 9;
             double res = ds.GetMultiplySeries(startValue, stopValue);
-            double wait = -15.642;
+            double wait = -15.642; 
             Assert.AreEqual(wait, res);
         }
     }
