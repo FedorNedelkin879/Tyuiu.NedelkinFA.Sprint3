f920b5b4f539326da6b6d5060b7f286b65d5a17c
Федя Неделькин <fffedosss898@gmail.com>
2024-11-27 20:39:08 +0500

    rtyuk

--- Tyuiu.NedelkinFA.Sprint3.Task2.V28.Lib/DataService.cs
+++ Tyuiu.NedelkinFA.Sprint3.Task2.V28.Lib/DataService.cs
@@ -7,16 +7,14 @@ namespace Tyuiu.NedelkinFA.Sprint3.Task2.V28.Lib
     {
         public double GetMultiplySeries(int startValue, int stopValue)
         {
-            double p = 1;
+            double p = 1; 
             int k = 1;
 
             do
             {
-                p *= Math.Pow(2, k) / (k + 1);
-                k++;
+                p *= Math.Pow(k + 1, 2) * Math.Cos(1.8); 
+                k++; 
             } while (k <= 9);
-
-            p *= Math.Cos(1.8);
             return Math.Round(p, 3);
         }
     }
