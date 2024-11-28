e99aebaa7f675710322c9fe49b5ddc371c534297
Федя Неделькин <fffedosss898@gmail.com>
2024-11-28 22:54:46 +0500

    фы

--- Tyuiu.NedelkinFA.Sprint3.Task5.V14.Test/DataServiceTest.cs
+++ Tyuiu.NedelkinFA.Sprint3.Task5.V14.Test/DataServiceTest.cs
@@ -15,7 +15,7 @@ namespace Tyuiu.NedelkinFA.Sprint3.Task5.V14.Test
             int startValue2 = 1;
             int stopValue2 = 14;
             double res = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
-            double wait = 282.762;
+            double wait = 231.235;
             Assert.AreEqual(wait, res);
         }
     }
