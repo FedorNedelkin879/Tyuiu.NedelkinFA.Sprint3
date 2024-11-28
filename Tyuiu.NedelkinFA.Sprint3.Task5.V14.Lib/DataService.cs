82c4e69deaf79127f7d219aa3bd38737c205ec0e
Федя Неделькин <fffedosss898@gmail.com>
2024-11-28 23:33:46 +0500

    ваорпаолрдло.

--- Tyuiu.NedelkinFA.Sprint3.Task5.V14.Lib/DataService.cs
+++ Tyuiu.NedelkinFA.Sprint3.Task5.V14.Lib/DataService.cs
@@ -8,15 +8,14 @@ namespace Tyuiu.NedelkinFA.Sprint3.Task5.V14.Lib
         {
             double sumSeries = 0;
             int i, j;
-            for (i = startValue1; i <= stopValue1; i++)  // Внешний цикл
+            for (i = startValue1; i <= stopValue1; i++)  // Внешний цикл по i
             {
-                for (j = startValue2; j <= stopValue2; j++)  // Внутренний цикл
+                for (j = startValue2; j <= stopValue2; j++)  // Внутренний цикл по j
                 {
-                    // Формула с sin(x) + 2/j
-                    sumSeries += Math.Sin(x) + (2.0 / j);  // Здесь мы используем sin(x) + 2/j
+                    sumSeries += Math.Sin(x) + (2.0 / j);  // Формула sin(x) + 2/j
                 }
             }
-            return Math.Round(sumSeries, 3);  // Округляем результат
+            return Math.Round(sumSeries, 3);  // Округляем результат до 3 знаков
         }
     }
 }
