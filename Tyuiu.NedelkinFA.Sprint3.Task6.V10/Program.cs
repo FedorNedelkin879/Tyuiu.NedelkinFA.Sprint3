c213937aa6ae1b664b8f86d8307ce1f0fb167568
Федя Неделькин <fffedosss898@gmail.com>
2024-11-29 00:01:56 +0500

    awrty

---
+++ Tyuiu.NedelkinFA.Sprint3.Task6.V10/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.NedelkinFA.Sprint3.Task6.V10.Lib;
+
+DataService ds = new DataService();
+int startValue = 10;
+int stopValue = 15;
+Console.WriteLine("nach otr " + startValue);
+Console.WriteLine("konec otr " + stopValue);
+Console.WriteLine("sum del = " + ds.GetSumTheDivisors(startValue, stopValue));
+Console.ReadKey();
