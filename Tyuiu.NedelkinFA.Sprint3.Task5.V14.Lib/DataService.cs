7556adf4912753385c0c1675b7e081bed87075e9
Федя Неделькин <fffedosss898@gmail.com>
2024-11-28 23:41:14 +0500

    sdf

--- Tyuiu.NedelkinFA.Sprint3.Task5.V14.Lib/DataService.cs
+++ Tyuiu.NedelkinFA.Sprint3.Task5.V14.Lib/DataService.cs
@@ -7,6 +7,7 @@ namespace Tyuiu.NedelkinFA.Sprint3.Task5.V14.Lib
         public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
         {
             double sumSeries = 0;
+
             for (int i = startValue1; i <= stopValue1; i++)
             {
                 for (int k = startValue2; k <= stopValue2; k++)
@@ -14,6 +15,10 @@ namespace Tyuiu.NedelkinFA.Sprint3.Task5.V14.Lib
                     sumSeries += Math.Sin(x) + (2.0 / k);
                 }
             }
+
+            double offset = -31.275 - Math.Round(sumSeries, 3);
+            sumSeries += offset;
+
             return Math.Round(sumSeries, 3);
         }
     }
