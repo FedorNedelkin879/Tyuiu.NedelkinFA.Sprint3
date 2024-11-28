82c99bce22baaaccccaee5d77cfbdade50a69fcc
Федя Неделькин <fffedosss898@gmail.com>
2024-11-28 22:54:16 +0500

    выаы

---
+++ Tyuiu.NedelkinFA.Sprint3.Task5.V14.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.NedelkinFA.Sprint3.Task5.V14.Lib
+{
+    public class DataService : ISprint3Task5V14
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sumSeries = 0;
+            int i, j;
+            for (i = startValue1; i <= stopValue1; i++)
+            {
+                for (j = startValue2; j <= stopValue2; j++)
+                {
+                    sumSeries = sumSeries + ((1 / Math.Sin(j)) + x);
+                }
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.NedelkinFA.Sprint3.Task5.V14/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.NedelkinFA.Sprint3.Task5.V14.Lib;
+
+DataService ds = new DataService();
+int x = 5;
+int startValue1 = 1;
+int stopValue1 = 5;
+int startValue2 = 1;
+int stopValue2 = 10;
+Console.WriteLine("x " + x);
+Console.WriteLine("start 1 = " + startValue1);
+Console.WriteLine("end 1 = " + stopValue1);
+Console.WriteLine("start 2 = " + startValue2);
+Console.WriteLine("end 2 = " + stopValue2);
+Console.WriteLine("sum = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+Console.ReadKey();
