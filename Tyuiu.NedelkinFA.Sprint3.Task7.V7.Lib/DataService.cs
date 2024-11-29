df40b802f36bebfc2757a453321d9fb26ab811bb
Федя Неделькин <fffedosss898@gmail.com>
2024-11-29 21:37:34 +0500

    вапр

--- Tyuiu.NedelkinFA.Sprint3.Task7.V7.Lib/DataService.cs
+++ Tyuiu.NedelkinFA.Sprint3.Task7.V7.Lib/DataService.cs
@@ -13,12 +13,13 @@ namespace Tyuiu.NedelkinFA.Sprint3.Task7.V7.Lib
 
             for (int x = startValue; x <= stopValue; x++)
             {
-                double term2 = (x + 2) != 0 ? Math.Cos(x) / (x + 2) : 0;
+                double term2 = (x + 2) != 0 ? Math.Cos(x) / (x + 2) : 0; 
                 double y = Math.Cos(x) + term2 - 3 * x;
                 valueArray[count] = Math.Round(y, 2);
                 count++;
             }
             return valueArray;
         }
+
     }
 }
