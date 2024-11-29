6f8decbf7f80cf11eecc62219c3b4e55d64dab42
Федя Неделькин <fffedosss898@gmail.com>
2024-11-29 21:31:19 +0500

    fghjkl

---
+++ Tyuiu.NedelkinFA.Sprint3.Task7.V7/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.NedelkinFA.Sprint3.Task7.V7.Lib;
+
+DataService ds = new DataService();
+int startValue = -5;
+int stopValue = 5;
+Console.WriteLine("start = " + startValue);
+Console.WriteLine("stop = " + stopValue);
+int len = ds.GetMassFunction(startValue, stopValue).Length;
+double[] = valueArray;
+valueArray = new double[len];
+valueArray = ds.GetMassFunction(startValue, stopValue);
+Console.WriteLine("x      |      f(x)");
+for (int i = 0; i <= len-1; i++)
+{
+    Console.WriteLine("{0,5:d}   |    {1, 5:f2} ", startValue, valueArray[i]);
+    startValue++;
+}
+Console.ReadKey();
