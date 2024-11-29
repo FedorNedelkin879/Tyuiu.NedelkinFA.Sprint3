e7088ccf5e648519f1ad6755406cb6a7970a2847
Федя Неделькин <fffedosss898@gmail.com>
2024-11-29 21:36:12 +0500

    ыва

--- Tyuiu.NedelkinFA.Sprint3.Task7.V7.Lib/DataService.cs
+++ Tyuiu.NedelkinFA.Sprint3.Task7.V7.Lib/DataService.cs
@@ -9,12 +9,13 @@ namespace Tyuiu.NedelkinFA.Sprint3.Task7.V7.Lib
             double[] valueArray;
             int len = (stopValue - startValue) + 1;
             valueArray = new double[len];
-            double y;
             int count = 0;
+
             for (int x = startValue; x <= stopValue; x++)
             {
-                y = Math.Round(Math.Sin(x), 2);
-                valueArray[count] = y;
+                double term2 = (x + 2) != 0 ? Math.Cos(x) / (x + 2) : 0;
+                double y = Math.Cos(x) + term2 - 3 * x;
+                valueArray[count] = Math.Round(y, 2);
                 count++;
             }
             return valueArray;
